95392dab0bc6264c716eb0c389994913306993c5
vibudh2209 <vibudh2209@gmail.com>
2017-11-24 11:30:09 -0800

    To detect if not character or factor

--- R/fact_orig.r
+++ R/fact_orig.r
@@ -11,6 +11,7 @@
 #' @examples
 #' fact_orig(c("This","is","test","examp"))
 fact_orig <- function(x){
+	if(!any(all(is.character(x)),all(is.factor(x))))stop("Input vector is neither character nor factor")
 	x_new <- unique(x)
 	ret<-fact_reord(x_new,c(1:length(x_new)))
 	return(factor(x,levels=levels(ret)))
